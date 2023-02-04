439a6282d88a04a812a106a5392b49f0e8b8bc7f
Patricia Correia <[blossomdeveloper08@gmail.com]>
2023-02-04 23:44:10 +0000

    chore: testing

---
+++ Projects/javascript.js
@@ -0,0 +1,36 @@
+let a = 22;
+let b = 45;
+
+if (a < b) {
+    console.log(b);
+}
+
+
+import { createSlice, configureStore } from "@reduxjs/toolkit";
+
+const counterSlice = createSlice({
+    name: 'counter',
+    initialState: {
+        value: 0
+    }, 
+    reducers: {
+        incremented: state => {
+            state.value += 1
+        },
+        decremented: state=> {
+            state.value -= 1
+        }
+    }
+})
+
+export const { incremented, decremented} = counterSlice.actions
+
+const store = configureStore({
+    reducer: counterSlice.reducer
+})
+
+store.subscribe(() => console.log(store.getState()))
+
+store.dispatch(incremented())
+store.dispatch(incremented())
+store.dispatch(decremented())
